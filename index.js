3a989d3101f8efe2773ae529cc63aa0738490c06
salam46khan <salam46.khan@gmail.com>
2023-10-21 03:32:40 +0600

    create and read product

--- index.js
+++ index.js
@@ -5,6 +5,41 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
+// hard data for brand 
+const brand = [
+    {
+      "id": 1,
+      "brand": "Toyota",
+      "image": "https://i.ibb.co/rFGK6YX/Toyota-removebg-preview.png"
+    },
+    {
+      "id": 2,
+      "brand": "Ford",
+      "image": "https://i.ibb.co/PQGdg3S/ford-removebg-preview.png"
+    },
+    {
+      "id": 3,
+      "brand": "BMW",
+      "image": "https://i.ibb.co/rfbf8p8/bmw-removebg-preview.png"
+    },
+    {
+      "id": 4,
+      "brand": "Mercedes-Benz",
+      "image": "https://i.ibb.co/fHT481P/Mercedes-Benz-removebg-preview.png"
+    },
+    {
+      "id": 5,
+      "brand": "Tesla",
+      "image": "https://i.ibb.co/QC3Nrqm/tesla-removebg-preview.png"
+    },
+    {
+      "id": 6,
+      "brand": "Honda",
+      "image": "https://i.ibb.co/88Nk2B5/honda-removebg-preview.png"
+    }
+  ]
+  
+
 // middle ware
 app.use(cors())
 app.use(express.json())
@@ -27,19 +62,38 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productCollection = client.db("productDB").collection('product')
+
+    app.get('/products', async (req, res)=>{
+        const cursor = productCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+    
+    app.post('/products', async (req, res) =>{
+        const product = req.body;
+        console.log(product);
+        const result = await productCollection.insertOne(product);
+        res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 
 
-
+app.get('/brand', (req, res)=>{
+    res.send(brand)
+})
 
 app.get('/', (req, res)=>{
     res.send('auto majesty is running')
